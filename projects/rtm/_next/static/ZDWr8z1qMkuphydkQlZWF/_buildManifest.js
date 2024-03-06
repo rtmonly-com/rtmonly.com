d2762d2181935a19c6bea74a4c4f401ad6b5e5db
adrian <161563017+rtmonly@users.noreply.github.com>
2024-03-06 00:29:51 -0800

    Add files via upload

---
+++ projects/rtm/_next/static/ZDWr8z1qMkuphydkQlZWF/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = function(s, e, a, c) {
+    return {
+        __rewrites: {
+            beforeFiles: [],
+            afterFiles: [],
+            fallback: []
+        },
+        "/": [s, a, e, c, "static/chunks/pages/index-db9b1d8d6d7df295.js"],
+        "/404": [s, a, e, c, "static/chunks/pages/404-f968c7019c8ab575.js"],
+        "/_error": ["static/chunks/pages/_error-54de1933a164a1ff.js"],
+        sortedPages: ["/", "/404", "/_error"]
+    }
+}("static/chunks/2cca2479-182ab63846a5314b.js", "static/chunks/870-8f2c03214346065b.js", "static/chunks/c16184b3-a38fca5b5583c035.js", "static/chunks/664-339c0d1773136295.js"),
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
